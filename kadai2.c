2b4a4e389da4f5c5aed78ce936a15bb1a92843d6
Dandelion-mm <balius.1064@gmail.com>
2017-04-20 15:55:08 +0900

    Change kadai2.c

--- kadai2.c
+++ kadai2.c
@@ -17,9 +17,11 @@
  *点をPointとして定義する
  */
 typedef struct{
-  double x, y;    //x座標、y座標
-  int w, a;    //wはその点が持つ重み
-  int name;    //nameは配列の名前（番号）
+  double x;      //x座標
+  double y;      //y座標
+  int weight;    //その点が持つ重み
+  int pPoint;    //この点の前の点情報
+  int name;      //配列の名前（番号）
 } Point;
 
 /**
@@ -47,15 +49,16 @@ int main(void){
   scanf("%d",&n);
   
   /** 乱数を発生させ、座標上の位置を決める */
-  int i, j, k,v;    //for文用の変数
-  Point p[n];    //Point型の配列p
+  int i, j, k;     //for文用の変数
+  int dp = n-1;    //目的地
+  Point p[n];      //Point型の配列p
   
   srand(time(NULL));
 
   for(i=0; i<n; i++){
     p[i].x = (double)rand() / RAND_MAX;
     p[i].y = (double)rand() / RAND_MAX;
-    p[i].w = 0;    //全地点の重さの初期値は0である
+    p[i].weight = 0;    //全地点の重さの初期値は0
     p[i].name = i;
   }
 
@@ -74,7 +77,7 @@ int main(void){
   printf("\n**************************************************\n");
   printf("ある２点（Ａ、Ｂ）を発生させた乱数の中から決めます\n");
   printf("A = (%f,%f) とします\n", p[0].x, p[0].y);
-  printf("B = (%f,%f) とします\n", p[n-1].x, p[n-1].y);
+  printf("B = (%f,%f) とします\n", p[dp].x, p[dp].y);
   printf("**************************************************\n\n");
 
   /** 
@@ -86,12 +89,13 @@ int main(void){
   for(i=1; i<n; i++){
     dist = getDistance(p[0].x, p[0].y, p[i].x, p[i].y);
       if(dist <= r){
-	p[i].w = 1;
-	p[i].a = 0;
+	p[i].weight = 1;    //重みに１を足す
+	p[i].pPoint = 0;    //前の点はスタートであるＡ(0)
+	//printf("%dの前は%d\n",i ,0);
       }
   }
 
-  printf("-----\n");
+  //printf("-----\n");
   
   /**
    *重みが１の点から、順々に距離を測り距離r以下の点を探す
@@ -100,42 +104,44 @@ int main(void){
   for(i=1; i<n; i++){
     for(j=1; j<n-1; j++){
       //重みが１の点から検索をする
-      if(p[j].w == i){
+      if(p[j].weight == i){
 	for(k=2; k<n; k++){
 	  dist = getDistance(p[j].x, p[j].y, p[k].x, p[k].y);
 	  //printf("%f\n",dist);
 	  if(dist <= r && dist != 0){
-	    if(p[k].w == 0 || p[j].w + 1 < p[k].w){
-	      p[k].w = p[j].w + 1;
-	      //printf("点%dの重み --> %d\n", p[k].name,  p[k].w); 
-	      p[k].a = j;
-	      printf("%dの前は%d\n", k , p[k].a);
-	      
+	    if(p[k].weight == 0 || p[j].weight + 1 < p[k].weight){
+	      p[k].weight = p[j].weight + 1;
+	      p[k].pPoint = j;
+	      //printf("%dの前は%d\n", k , p[k].pPoint);
 	    }
 	  }
-	  if(p[n-1].w > 0){
+	  if(p[dp].weight > 0){
 	    break;
 	  }
 	}
       }
-      if(p[n-1].w > 0){
+      if(p[dp].weight > 0){
 	break;
       }
     }
-    if(p[n-1].w > 0){
+    if(p[dp].weight > 0){
       break;
     }
   }
 
-  printf("点Bの重さは%d\n",p[n-1].w);
+  printf("-----\n");
+  printf("点Bの重さは%d\n",p[dp].weight);
   
-  //出力お試
-  v=n-1;
-  for(i=0; i<p[n-1].w; i++){
-    printf("%d\n",v);
-    v=p[v].a;
+  /** 出力 */
+  printf("-----\n");
+  printf("B(%f,%f) <-- ", p[dp].x ,p[dp].y);
+  
+  j = p[dp].pPoint;
+  for(i=0; i<p[dp].weight - 1; i++){
+    printf("(%f,%f)", p[j].x ,p[j].y);
+    j = p[j].pPoint;
+    printf(" <-- ");
   }
-
-
+  printf("A(%f,%f)\n", p[0].x ,p[0].y);
   
 }
