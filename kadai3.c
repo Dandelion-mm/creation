6bf4b4bbc7cc681788f617898fb4cc8d14660260
Dandelion-mm <balius.1064@gmail.com>
2017-04-27 17:02:41 +0900

    Change kadai3.c

--- kadai3.c
+++ kadai3.c
@@ -2,7 +2,7 @@
  *
  * kadai3.c
  *
- * Created on : 2017/04/26
+ * Created on : 2017/04/28
  *     Author : murota
  *
  */
@@ -17,11 +17,18 @@
  *点をPointとして定義する
  */
 typedef struct{
-  double x;      //x座標
-  double y;      //y座標
-  int weight;    //その点が持つ重み
-  int pPoint;    //この点の前の点情報
-  int name;      //配列の名前（番号）
+  int name;        //配列の名前（番号
+  double sx;       //移動前のx座標
+  double sy;       //移動前のy座標
+  double mx;       //移動中のx座標
+  double my;       //移動中のy座標
+  double dx;       //目的地のx座標
+  double dy;       //目的地のy座標
+  double speed;    //移動速度
+  double cos;      //目的地と移動前との正弦
+  double sin;      //目的地と移動前との余弦
+  int weight;      //その点が持つ重み
+  int pPoint;      //この点の前の点情報
 } Point;
 
 /**
@@ -38,7 +45,22 @@ double getDistance(double px, double py, double qx, double qy) {
 }
 
 /**
- * Mainの実行
+ * A,Bの値を出力する関数
+ * @param ax Aのx座標
+ * @param ay Aのy座標
+ * @param bx Bのx座標
+ * @param by Bのy座標
+ */
+void PrintAandB(double ax, double ay, double bx, double by){
+  printf("\n**************************************************\n");
+  printf("ある２点（Ａ、Ｂ）を発生させた乱数の中から決めます\n");
+  printf("A = (%f,%f) とします\n", ax, ay);
+  printf("B = (%f,%f) とします\n", bx, by);
+  printf("**************************************************\n\n");
+}
+
+/**
+ * Main関数
  */
 int main(void){
   
@@ -49,31 +71,21 @@ int main(void){
   scanf("%d",&n);
   
   /** 乱数を発生させ、座標上の位置を決める */
-  int i, j, k;            //for文用の変数
-  int dp = n-1;     //目的地
-  Point p[n];        //Point型の配列p
+  int i, j, k;     //for文用の変数
+  int sp = 0;      //点A
+  int dp = n-1;    //点B
+  Point p[n];      //Point型の配列P
   
   srand(time(NULL));
   
   for(i=0; i<n; i++){
-    p[i].x = (double)rand() / RAND_MAX;
-    p[i].y = (double)rand() / RAND_MAX;
+    p[i].sx = (double)rand() / RAND_MAX;
+    p[i].sy = (double)rand() / RAND_MAX;
     p[i].weight = 0;    //全地点の重さの初期値は0
     p[i].name = i;
   }
   
-  /**
-   * 図を作成するために
-   * 乱数を別ファイルに保存しておく
-   */
-  FILE *file;
-  file = fopen("point.txt","w");
-  for(i=0; i<n; i++){
-    fprintf(file,"%f %f\n",p[i].x,p[i].y);
-  }
-  fclose(file);
-  
-  /** rの距離の設定 */
+  /** 距離rの設定 */
   double r = 0;
   
   do{
@@ -85,11 +97,7 @@ int main(void){
   }while (r <= 0 || 1 <= r);
   
   /** A、Bを設定する  */
-  printf("\n**************************************************\n");
-  printf("ある２点（Ａ、Ｂ）を発生させた乱数の中から決めます\n");
-  printf("A = (%f,%f) とします\n", p[0].x, p[0].y);
-  printf("B = (%f,%f) とします\n", p[dp].x, p[dp].y);
-  printf("**************************************************\n\n");
+  PrintAandB(p[sp].sx, p[sp].sy, p[dp].sx, p[dp].sy);
   
   /** 
    * Aからの距離をそれぞれ求める
@@ -98,7 +106,7 @@ int main(void){
   double dist = 0;    //２点間の距離を格納する変数
   
   for(i=1; i<n; i++){
-    dist = getDistance(p[0].x, p[0].y, p[i].x, p[i].y);
+    dist = getDistance(p[0].sx, p[0].sy, p[i].sx, p[i].sy);
     if(dist <= r){
       p[i].weight = 1;    //重みに１を足す
       p[i].pPoint = 0;    //前の点はスタートであるＡ(0)
@@ -112,7 +120,7 @@ int main(void){
    * Bに重みがついた時点で、最短経路が確定する
    */
   
-  for(i=1; i<n; i++){
+  /*for(i=1; i<n; i++){
     for(j=1; j<n-1; j++){
       //重みが１の点から検索する
       if(p[j].weight == i){
@@ -136,10 +144,10 @@ int main(void){
     if(p[dp].weight > 0){
       break;
     }
-  }
-    
-    /** 出力メソッド */
-  if(p[dp].weight != 0){
+    }*/
+
+  /** 出力メソッド */
+  /*if(p[dp].weight != 0){
     printf("AからBへの最短経路は\n\n");
     printf("B(%f,%f)\n", p[dp].x ,p[dp].y);
     printf(" <-- ");
@@ -153,5 +161,28 @@ int main(void){
     printf("A(%f,%f)\n\n", p[0].x ,p[0].y);
   }else{
     printf("AとBはつながりませんでした\n\n");
-  }
+    }*/
+
+  
+  /** 点の移動を考える */
+  double dTime = 0.01;
+
+  /** 目的地 */
+  p[sp].dx = (double)rand() / RAND_MAX;
+  p[sp].dy = (double)rand() / RAND_MAX;
+
+  p[sp].speed = 0.01;
+
+  printf("Aの移動先(%f,%f)\n\n", p[sp].dx, p[sp].dy);
+
+  p[sp].cos = abs((p[sp].dx - p[sp].sx)) / getDistance(p[sp].sx, p[sp].sy, p[sp].dx, p[sp].dy);
+  p[sp].sin = abs((p[sp].dy - p[sp].sy)) / getDistance(p[sp].sx, p[sp].sy, p[sp].dx, p[sp].dy);
+  
+  p[sp].mx = p[sp].sx + p[sp].speed * p[sp].cos;
+  p[sp].my = p[sp].sy + p[sp].speed * p[sp].sin;
+
+  printf("%f,%f\n\n", p[sp].mx, p[sp].my);
+  
+  
+  
 }
